0c4f69450b0e7fba614caad87ea3f98641891372
Eric Prom <eric@kobidigital.com>
2018-03-26 21:19:57 +0700

    family-trip init

---
+++ family-trip.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+const moment = require('moment');
+
+const baseUrl = 'http://dataservice.accuweather.com';
+const apiKey = 'S1BrKQvtCrlQ2EIxflcmKZlkemjUAcsS';
+
+
+function encodeQueryData(data) {
+   let ret = [];
+   for (let d in data)
+     ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
+   return ret.join('&');
+}
+
+function getLocationKey(){
+  let latlng = '13.7978215,100.5239869';
+  var data = { 'apikey': apiKey, 'q': latlng, 'language': 'th-th' };
+  var querystring = encodeQueryData(data);
+  axios.get(baseUrl+'/locations/v1/cities/geoposition/search?'+querystring)
+    .then(response => {
+      var locKey = response.data.Key;
+      forecasts(locKey);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+function forecasts(locKey){
+  var data = { 'apikey': apiKey, 'language': 'th-th', 'metric' :true };
+  var querystring = encodeQueryData(data);
+  axios.get(baseUrl+'/forecasts/v1/daily/5day/'+locKey+'?'+querystring)
+    .then(response => {
+      for (var i = response.data.DailyForecasts.length - 1; i >= 0; i--) {
+        var data = response.data.DailyForecasts[i];
+        console.log("วันที่:",moment(data.Date).format('DD/MM/YYYY'));
+        console.log("กลางวัน:",data.Day.IconPhrase);
+        console.log("กลางคืน:",data.Night.IconPhrase);
+        console.log("อุณหภูมิ:",data.Temperature.Minimum.Value+'-'+data.Temperature.Maximum.Value);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+getLocationKey();
